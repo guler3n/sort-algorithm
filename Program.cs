965ec2e6f19be4a323d43fd555ad7ae3a4531d7f
enesgul3r <56351748+enesgul3r@users.noreply.github.com>
2023-08-19 16:47:59 +0300

    InsertionSort
    There is a example to Insertion Sort algorithm about series.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Program
+{
+    public class InsertionSort
+    {
+        public static void Main(string[] args)
+        {
+            var series = new int[] { 7, 3, 5, 8, 2, 9, 4, 15, 6 };
+            var tempSeries = new int[series.Length];
+            tempSeries[0] = series.FirstOrDefault();
+            int counter = 0;
+            while (counter + 1 < series.Length)
+            {
+                counter++;
+                tempSeries[counter] = series[counter];
+                for (int j = counter - 1; j >= 0; j--)
+                {
+                    if (tempSeries[j + 1] < tempSeries[j])
+                    {
+                        var temp = tempSeries[j + 1];
+                        tempSeries[j + 1] = tempSeries[j];
+                        tempSeries[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
